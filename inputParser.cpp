0302b3577b2bf063f8da39e94c3d5ec50a981f9b
UnbreakablePotato <swipeytwit@gmail.com>
2025-11-10 20:29:07 +0100

    Deleted changes to inputParser.

--- inputParser.cpp
+++ inputParser.cpp
@@ -4,11 +4,19 @@
 std::vector<std::string> parser(std::string string) {
     std::stringstream ss(string);
     std::vector<std::string> inputParsed;
+    std::vector<std::string> stringParsed;
     std::string word;
 
-    while(ss >> word)
+    while(ss >> word /*&& word != "\""*/)
     {
         inputParsed.push_back(word);
     }
+
+    /*if(word == "\""){
+        while(word!="\""){
+            stringParsed.push_back(word);
+        }
+        return stringParsed;
+    }*/
     return inputParsed;
 }
